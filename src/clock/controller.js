76db20abfa3dd7cc07bf54393aa06408ff332cf5
Julian Andrade <julian@ndrade.com.br>
2019-11-04 17:57:33 -0300

    clock/controller fix #hotfix

--- src/clock/controller.js
+++ src/clock/controller.js
@@ -1,18 +1,20 @@
 module.exports = (()=>{
-    const Clock = require('./business/Clock');
+	const Clock = require('./business/Clock');
 
-    function smallestClockAngle(req, res) {
-        const {hour, minute = 0} = req.params;
+	function smallestClockAngle(req, res, next) {
+		const {hour, minute = 0} = req.params;
 
-        try {
-            let angle = Clock.smallestAngleBetweenArrows(hour, minute);
-            res.status(200).json({angle: angle});
-        } catch(err) {
-            res.status(500).json({error: err.message});
-        }
-    }
+		try {
+			let angle = Clock.smallestAngleBetweenArrows(hour, minute);
+			res.status(200).json({angle: angle});
+		} catch(err) {
+			res.status(500).json({error: err.message});
+		} finally {
+			next();
+		}
+	}
 
-    return {
-        smallestClockAngle: smallestClockAngle
-    }
+	return {
+		smallestClockAngle: smallestClockAngle
+	}
 })();
